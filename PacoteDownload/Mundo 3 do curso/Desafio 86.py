071d9ff3ebc4d4619aabefcc0255a9e53f52eb9c
Gabriel Evangelista Rodrigues <gabrielevangelista2.0@gmail.com>
2021-03-03 12:40:37 -0300

    Exercícios

--- PacoteDownload/Mundo 3 do curso/Desafio 86.py
+++ PacoteDownload/Mundo 3 do curso/Desafio 86.py
@@ -1,28 +1,25 @@
-matriz = [[], [], []]
+'''matriz = [[], [], []]
 
-for l in range (1,4):
-    for c in range(1,4):
-        entrada = int(input(f"Digite o elemento {l},{c}: "))
-        if l == 1:
+for linha in range(1, 4):
+    for coluna in range(1, 4):
+        entrada = int(input(f"Digite o elemento {linha},{coluna}: "))
+        if linha == 1:
             matriz[0].append(entrada)
-        elif l == 2:
+        elif linha == 2:
             matriz[1].append(entrada)
         else:
             matriz[2].append(entrada)
+
 print(f'[{matriz[0][0]}] [{matriz[0][1]}] [{matriz[0][2]}]')
 print(f'[{matriz[1][0]}] [{matriz[1][1]}] [{matriz[1][2]}]')
-print(f'[{matriz[2][0]}] [{matriz[2][1]}] [{matriz[2][2]}]')
+print(f'[{matriz[2][0]}] [{matriz[2][1]}] [{matriz[2][2]}]')'''
+# Solução do guanabara:
+matrix=[[0,0,0], [0,0,0], [0,0,0]]
+for linha in range (3):
+    for coluna in range (3):
+        matrix[linha][coluna] = int(input(f'Valor para [{linha}, {coluna}]:'))
 
-
-
-
-'''for i in range(9):
-    entrada = int(input(f"Digite o elemento {l},{c}: "))
-    c += 1
-    if i < 3:
-        matriz[0].append(entrada)
-    elif i >= 3 and i < 6:
-        matriz[1].append(entrada)
-    else:
-        matriz[2].append(entrada)
-print(matriz)'''
+for linha in range(3):
+    for coluna in range(3):
+        print(f'[{matrix[linha][coluna]:^5}]', end='')
+    print()
